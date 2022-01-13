3149b15c1c0b76e485448930bd848352cae7b561
seokmin <6863829+essemfly@users.noreply.github.com>
2022-01-13 12:27:27 +0900

    Add cors

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/99designs/gqlgen v0.14.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-chi/chi v1.5.4
+	github.com/rs/cors v1.6.0
 	github.com/tkanos/gonfig v0.0.0-20210106201359-53e13348de2f
 	github.com/vektah/gqlparser/v2 v2.2.0
 	go.mongodb.org/mongo-driver v1.8.2
--- server.go
+++ server.go
@@ -6,9 +6,12 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/lessbutter/habit-tracker-api/auth"
 	"github.com/lessbutter/habit-tracker-api/config"
 	"github.com/lessbutter/habit-tracker-api/graph"
 	"github.com/lessbutter/habit-tracker-api/graph/generated"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -18,7 +21,26 @@ func main() {
 	conf := config.GetConfiguration()
 	config.InitDB(conf)
 
+	router := chi.NewRouter()
+	router.Use(auth.Middleware())
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	// srv.AddTransport(&transport.Websocket{
+	// 	Upgrader: websocket.Upgrader{
+	// 		CheckOrigin: func(r *http.Request) bool {
+	// 			// Check against your desired domains here
+	// 			return r.Host == "localhost:8080"
+	// 		},
+	// 		ReadBufferSize:  1024,
+	// 		WriteBufferSize: 1024,
+	// 	},
+	// })
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
